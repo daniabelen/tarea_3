ea99095035f0de7acb289c0ceaf76ad1c5dd6df0
dmunoz16 <dania.m.1607@gmail.com>
2019-09-30 20:46:26 -0300

    tarea_3 terminada

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# tarea_3
+﻿# tarea_3
 Guía de listas y FOR-LOOP
+
+-lo primero fue crear una lista con 11 elementos numéricos,luego para acceder a un elemento se utilizó: listaDeNumeros[5] lo cual me da que en la pocision 5 esta el 1.
+
+*Ejercicio 1:*
+al cambiar el numero 5 dentro de la función listaDeNumeros[] por un 0, me entrega una lista vacia ya que en la lista creada no existe la posicion 0, luego al poner un numero negativo, que en mi caso fue un -4 me entrega los coeficientes correspondiente a cada posicion dentro de la lista pero suprimeindo el valor correspondiente al orden ejecutado, es decir se salta el valor de la posicion 4.
+
+
+*Ejercicio 2:* 
+
+unlist sirve para cambiar el formato del valor, desde la posicion a un valor numerico como tal. en el primer if que utilice la función estaba mala por causa de un parentesis, esto hace que no sea un argumento numérico por eso me arroja error, en cambion en el segundo if si se cumple el unlist dado que son armumentos numéricos dandome como resultado "se cumple". y al usar listaDeNumeros[5] me dio [[1]] sobre [1]1 pero al utilizar unlist(listaDeNumeros[5]) me entrego solo [1]1.
+
+
+*Ejercicio 3:* 
+
+al ejecutar listaDeNumeros[5]<-12 me cambia en valor de la posicion 5 que era 1 por el nuevo valor que ahora es 12.
+
+*Ejercicio 4:* 
+
+para que funcione el ejemplo de la funcion llamada lenght, hay que poner el nombre de la list que se creó que es listaDeNumeros en donde estada nombre_variable (length(nombre_variable)) quedando length(listaDeNumeros) y así me dió la longuitud que tiene la lista, la cual corresponde a 11, ya que listaDeNumeros contiene 11 elementos.
+
+*Ejercicio 5:*
+
+al poner valorInicial<-5 me crea una variable con un valor 5, y con valorFinal<-20 me da una variable con valor 20, al poner valorInicial : valorFinal me entrega una lista desde 5 hasta 20 que son los valores que asigné, luego al cambiar los valores a las variables creadas, asignando el 5 como valorFinal y 20 como valorInicial, y volver a poner valorInicial : valorFinal me invierte la lista dado que cambie el orden de los valores. luego al aplicar: "valorInicial : length(listaDeNumeros)" me entrega una lista con los mismos primeros 10 elementos de la lista anterior que hice la ultima vez que utilice: valorInicial : valorFinal.  
+
+
+*Ejercicio 6:*
+
+utilizando for se crea un listado asignando a "i" un valor desde 1 hasta 100,luego en print entrega una lista hacia bajo en donde cambia el "i" desde el numero 1 hasta el numero 100 entre las palabras "cuento" y "misisipis".
+
+*Ejercicio 7:*
+
+adapté el  codigo anterir por :for(i in listaDeNumeros){print(paste("elemento",i," de ListaDeNumeros"))} donde ahora se cambio a "i" por los elemntos de la listaDeNumeros crado anteriormente y me dió un listado hacia abajo donde cambia el "i" por los elemento que estan dentro de la listaDenumeros manteneindo las palabras "elemento", y "de ListaDeNumeros".
+
+*EJERCICIO 8:* 
+
+utilice el siguiente codigo: for (i in listaDeNumeros){
+  print(i)
+  if(i%%2==0) {print("par")} else {print("impar")}
+}
+y lo que hizo fue entregarme la informacion correspondiente a cada elemento de la lista mencionando si este es par o inpar, por ejemplo: [1]2 "par". 
+
+
+*Ejercicio 9:* 
+
+para determinar las deciciones se utilizaron los siguientes codigos:
+Determinar_Ganador<-function(total,votosSI,votosNO){
+  if (votosSI+votosNO>total){
+    "votaciones alteradas"
+  }else if (votosSI>=votosNO && votosSI>=0.3*total){
+    paste ("SI")
+  }else {
+    paste ("NO")
+  }
+}
+Determinar_Ganador(98,99,55)
+Determinar_Ganador(99,96,3)
+Determinar_Ganador(100,5,95)
+al querer determinar la funcion se consiguió los siguientes resultados:votaciones alteradas,SI,NO respectivamente para cada una de las funciones de determinar_Ganador.
+
+*Ejercicio 10:* 
+
+ ejecuté las funciones de resta, multiplicacion y division asignando siempre los mismos valores para a y b que son 3 y 4 respectivamente,lo que hizo la función fue dar los resultados correspondientes al desalloro de dichas operaciones entres los dos coeficientes que son 3 y 4, es por eso que se obtuvieron los siguientes resultados,: resta:-1,multiplicacion:12,division:0,75
+
+*Ejercicio 11:* 
+
+para el regtangulo realicé la funcion:
+Area_rectangulo<-function(b1,h1,b2,h2){
+  b1*h1-b2*h2
+}
+ luego le asigné valores a b1,h1,b2, y h2. los cuales corresponden a 4,6,2,3. y se llegó a que la diferencia entre las áreas de los dos rectangulos es de 18.
+para el circulo realicé la función: 
+Area_rectangulo(4,6,2,3)
+Area_circulo<-function(r1,r2){
+abs((pi*r1^2)-(pi*r2^2))
+}
+y los valores que le di a r1 y r2 fueron 5 y 3 dandome como resultado un area de 50,26548.
+
+*Ejercicio es Opcional:* Ejecute el siguiente código y diga cuales son las ventajas y desventajas de hacer un loop con lista o rango
+
+for( i in listaDeNumeros){
+  print(i)
+}
+
+al ejecutar el código me entrego un listado hacia abajo con los valores de la listaDeNumeros que se creo al comienzo, la ventaja es que al usa el for a todos los elementos de la lista los estoy llamamdo "i" entonces al poner "i" me ahorro de poner los datos manualmente, como se hizo en el ejercicio 6 y ,mas especificamente en el ejercicio 7, donde puse las palabaras "elemento", "de listaDeNumeros" y fue cambiando el "i" por los elemntos de la listaDeNumeros.
--- desarrollo preguntas.R
+++ desarrollo preguntas.R
@@ -1,13 +1,19 @@
-rm(a,b,d,e,x,X,x,y)
+
 listaDeNumeros <- list(2,5,6,2,1,5,6,10,11,20,15)
 listaDeNumeros[5]
+
 ###Ejercicio 1: ¿Qué pasa si reemplazamos el 5 por un 0?¿Qué pasa si en vez de poner un número positivo, menor igual al largo de la lista, colocamos un número mayor a la cantidad de elementos de la lista o un número negativo
+
 listaDeNumeros[0]
+
 ##como la posicion cero no existe, entrega una lista vacia. 
 listaDeNumeros[-4]
+
 ###al poner un numero negativo, me entrega el orden de la pisicion con el valor correspondiente, eliminando o saltandose el valor correspondiente a la posicion de valor que se puso en negativo.
 ##al reemplazar el 5 por un valor negativo entrega la lista pero suprimiento el valor que se encuentra en la posicion ejecutada.
+
 ##Ejercicio 2: A continuación se le presenta la función unlist, la cual se puede usar de la siguiente manera: unlist(listaDeNumeros[5]) ¿Qué diferencia existe entre usar o no el unlist, y cómo cree ud que le podría ayudar esta función?
+
 listaDeNumeros[5]
 unlist(listaDeNumeros[5])
 if(listaDeNumeros[5]+1>0) {
@@ -18,25 +24,108 @@ if(unlist(listaDeNumeros[5])+1>0){
   print("se cumple")
  }
 ###unlist sirve para cambiar el formato del valor, desde la posicion a un valor numerico como tal. en el primer if esta malo el comando por causa de un parentesis, esto hace que no sea un argumento numerico por eso arroja error en cambion en el segundo if si se cumple el unlist dado que son armumentos numericos 
+
 ###Ejercicio 3: ¿Qué pasa cuando realizamos la siguiente acción: listaDeNumeros[5] <- 12?
+
 listaDeNumeros[5]<-12
 listaDeNumeros[5]
+
 ### al ejecutar listaDeNumeros[5]<-12 me cambia en valor de la posicion 5 que era 1 por el nuevo valor que ahora es 12.
+
 ###Ejercicio 4: Existe una función llamada length, esta se usa de la siguiente manera: length(nombre_variable), ¿por qué debería ser cambiado nombre_variable para que funcione con el ejemplo que estamos trabajando (listaDeNumeros)? ¿qué da como resultado?
 length(listaDeNumeros)
+
 ### para que funcione el ejemplo de la funcion llamada lenght, hay que poner el nombre de la list que se creó y asi nos dara la longuitud que tiene la lista, la cual corresponde a 11, ya que contiene 11 elementos
+
 ###Ejercicio 5: Para generar secuencias se tiene un valor inicial y un valor final, donde ambos valores son enteros, para el ejemplo, y la sintaxis en R es la siguiente:
+
 valorInicial<-5 
 valorFinal<-20
 valorInicial : valorFinal
 valorInicial<-20 
 valorFinal<-5
 valorInicial : valorFinal
+
 ### al poner valorInicial<-5 me crea una variable con un valor 5, y con valorFinal<-20 me da una variable con valor 20, al poner valorInicial : valorFinal me entrega una lista desde 5 hasta 20 que son los valores que asigne, luego al cambiar los valores me invierte la lista dado que cambie los valores por los mismos pero en distinto orden.
+
 valorInicial : length(listaDeNumeros)
+
 ##se crea la misma lista que se creo cuando se uso valorInicial : valorFinal
+
 ###Ejercicio 6: Ejecute el ejemplo anterior y comente ¿Cuál es la condición establecida en el for, según el resultado que muestra la consola?
+
 for(i in 1 : 100){
   print(paste("cuento ",i," misisipis"))
 }
+
 #### se crea un listado,en donde cambia el "i" desde el numero 1 hasta el numero 100.
+
+###Ejercicio 7: Adapte el código anterior para que imprima los elementos de la lista que se creó anteriormente
+
+for(i in listaDeNumeros){print(paste("elemento",i," de ListaDeNumeros"))}
+
+###al adaptar el  codigo se creó un listado donde menciona elemento, y de ListaDeNumeros cambiando el i por los numeros correspondientes a los numeros dentro de la lista de numeros. 
+
+##EJERCICIO 8 : Usando los condicionales vistas en clases, adapte el código anterior para que muestre si un número es par o impar.
+
+for (i in listaDeNumeros){
+  print(i)
+  if(i%%2==0) {print("par")} else {print("impar")}
+}
+###entrega la información de los elemntos que estan dentro de listaDeNumeros, mencionando si son par o impar.
+
+
+###Ejercicio 9: Como vimos en clases anteriores el uso de condicionales es útil para tomar decisiones respecto a ciertas opciones que se presentan, para practicar esta metodología se le pida que resuelva el siguiente ejercicio.
+
+Determinar_Ganador<-function(total,votosSI,votosNO){
+  if (votosSI+votosNO>total){
+    "votaciones alteradas"
+  }else if (votosSI>=votosNO && votosSI>=0.3*total){
+    paste ("SI")
+  }else {
+    paste ("NO")
+  }
+}
+Determinar_Ganador(98,99,55)
+Determinar_Ganador(99,96,3)
+Determinar_Ganador(100,5,95)
+
+###al querer determinar la funcion se consiguió los siguientes resultados:votaciones alteradas,SI,NO respectivamente para cada una de las funciones.
+
+###Ejercicio 10: Una función es un grupo de instrucciones que toma valores de entradas para generar un resultado. Por ejemplo, si se crea una función que sume dos elementos, a y b, de la siguiente manera:
+
+#Suma<-function(a,b){a+b}
+##Y luego de esto ejecute lo siguiente en R:
+##Suma(3,4)
+
+Resta<-function(a,b){a-b}
+Resta(3,4)
+Multiplicacion<-function(a,b){a*b}
+Multiplicacion(3,4)
+Division<-function(a,b){a/b}
+Division(3,4)
+
+###al aplicar las funciones de resta, multiplicacion y division se obtuvo los resultados correspondientes al desalloro de dichas operaciones entres los dos coeficientes que son 3 y 4, es por eso que se obtuvieron los siguientes resultados,: resta:-1,multiplicacion:12,division:0,75
+
+###Ejercicio 11: Cree dos funciones, una que genere la diferencia entre las áreas de dos rectángulos(área sombreada figura 1) y otra de las áreas de un círculo (área sombreada figura 2), como muestran las siguientes figuras:
+
+Area_rectangulo<-function(b1,h1,b2,h2){
+  b1*h1-b2*h2
+}
+Area_rectangulo(4,6,2,3)
+Area_circulo<-function(r1,r2){
+abs((pi*r1^2)-(pi*r2^2))
+}
+
+Area_circulo(5,3)
+
+####al realizar las formulas se llego a que la diferencia entre las áreas de los dos rectangulos es de 18 y la de los circulos es de 50,26548.
+
+###Ejercicio es Opcional: Ejecute el siguiente código y diga cuales son las ventajas y desventajas de hacer un loop con lista o rango
+
+for( i in listaDeNumeros){
+  print(i)
+}
+
+###a la letra "i" le asigne los valores de la listaDeNumeros, entregandome un listado hacia abajo cuando presione print.
+
